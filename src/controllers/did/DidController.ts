8151c1dde753664f72a6548a4666de57e7d89a44
tarunvaddeofficial <tarun.vadde@ayanworks.com>
2023-06-13 12:10:43 +0530

    IndyError solved in DidController
    Signed-off-by: tarunvaddeofficial <tarun.vadde@ayanworks.com>
    Signed-off-by: KulkarniShashank <shashank.kulkarni@ayanworks.com>

--- src/controllers/did/DidController.ts
+++ src/controllers/did/DidController.ts
@@ -1,9 +1,8 @@
 import type { DidCreateOptions, DidResolutionResultProps } from '../types'
-import { JsonTransformer } from '@aries-framework/core'
+import { DidCreateResult, DidOperationStateActionBase, JsonTransformer, KeyType, TypedArrayEncoder } from '@aries-framework/core'
 import { Agent } from '@aries-framework/core'
 import { Body, Controller, Example, Get, Path, Post, Res, Route, Tags, TsoaResponse } from 'tsoa'
 import { injectable } from 'tsyringe'
-
 import { Did, DidRecordExample } from '../examples'
 
 @Tags('Dids')
@@ -42,17 +41,65 @@ export class DidController extends Controller {
    */
   // @Example<DidResolutionResultProps>(DidRecordExample)
 
-  @Post('/:write')
+  @Post('/write')
   public async writeDid(
     @Body() data: DidCreateOptions,
     @Res() internalServerError: TsoaResponse<500, { message: string }>) {
     try {
-      const resolveResult = await this.agent.dids.create(data);
+
+      console.log("STARTINGGG")
+      // testseed000000000006000000067890
+      // const resolveResult = await this.agent.dids.create({
+      //   "method": "indy",
+      //   "options": {
+      //     alias: "Hello",
+      //     submitterDid: 'did:indy:bcovrin:AtD4JwyJnd57MpuQ3BTDhy',
+      //     role: 'ENDORSER',
+      //   },
+      //   "secret": {
+      //     "privateKey": TypedArrayEncoder.fromString(data.seed)
+      //   }
+      // });
+      // console.log(JSON.stringify(resolveResult));
+      const key = await this.agent.wallet.createKey({
+        keyType: KeyType.Ed25519,
+        privateKey: TypedArrayEncoder.fromString(data.seed)
+      });
+      console.log("KEY CREATED");
+      const unqualifiedIndyDid = TypedArrayEncoder.toBase58(key.publicKey.slice(0, 16))
+      console.log("UNQUALIFIEDDID: ", unqualifiedIndyDid);
+      const resolveResult = await this.agent.dids.create({
+        method: 'indy',
+        options: {
+          alias: 'Hi',
+          submitterDid: `did:indy:bcovrin:${unqualifiedIndyDid}`,
+          role: 'ENDORSER'
+        },
+        // secret: {
+        //   privateKey: TypedArrayEncoder.fromString(data.seed)
+        // }
+      });
+      console.log("resolved: ---- ", JSON.stringify(resolveResult));
+      // if (resolveResult.didState.did) {
+      //   console.log("Inside IF");
+      //   const importing = await this.agent.dids.import({ did: resolveResult.didState.did, overwrite: true });
+      //   console.log("Done with ", importing);
+      // }
       return JsonTransformer.toJSON(resolveResult);
+
     }
     catch (error) {
       return internalServerError(500, { message: `something went wrong: ${error}` })
 
     }
   }
+
+  @Get('/')
+  public async getDids() {
+    const createdDids = await this.agent.dids.getCreatedDids({})
+    console.log("Created dids: ", createdDids);
+    return createdDids;
+  }
 }
+
+
