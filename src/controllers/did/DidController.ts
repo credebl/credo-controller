42f42fe67e866fc43353a458591d40277950fff1
KulkarniShashank <shashank.kulkarni@ayanworks.com>
2023-06-14 17:15:01 +0530

    Added response after DID register
    Signed-off-by: KulkarniShashank <shashank.kulkarni@ayanworks.com>
    Signed-off-by: Krishna Waske <krishna.waske@ayanworks.com>

--- src/controllers/did/DidController.ts
+++ src/controllers/did/DidController.ts
@@ -71,7 +71,7 @@ export class DidController extends Controller {
                 },
               ],
             })
-            return res.data;
+            return { did: `did:indy:bcovrin:${res.data.did}` };
           }
         })
     }
