a3d600b16de2f4835063fe4348eed8d9b0e4b547
KulkarniShashank <shashank.kulkarni@ayanworks.com>
2023-09-28 22:20:04 +0530

    refactor: Added and enhanceed the indicio:testnet
    Signed-off-by: KulkarniShashank <shashank.kulkarni@ayanworks.com>

--- src/controllers/did/DidController.ts
+++ src/controllers/did/DidController.ts
@@ -9,6 +9,7 @@ import * as IndySdk from 'indy-sdk';
 import axios from 'axios';
 import { IndyVdrDidCreateOptions, IndyVdrDidCreateResult } from '@aries-framework/indy-vdr'
 import { generateKeyPairFromSeed } from '@stablelib/ed25519'
+import { BCOVRIN_REGISTER_URL, INDICIO_NYM_URL } from '../../utils/util'
 
 @Tags('Dids')
 @Route('/dids')
@@ -53,148 +54,137 @@ export class DidController extends Controller {
   @Post('/write')
   public async writeDid(
     @Body() data: DidCreate,
-    @Res() internalServerError: TsoaResponse<500, { message: string }>) {
+    @Res() internalServerError: TsoaResponse<500, { message: string }>
+  ) {
     try {
 
-      data.method = data.method ? data.method : 'bcovrin';
-      if ('bcovrin' === data.method) {
-        let body = {
+      data.method = data.method || 'bcovrin:testnet';
+      const didMethod = `did:indy:${data.method}`;
+
+      if (data.method.includes('bcovrin')) {
+        const body = {
           role: 'ENDORSER',
           alias: 'Alias',
           seed: data.seed
         };
 
-      return await axios
-        .post('http://test.bcovrin.vonx.io/register', body)
-        .then(async (res) => {
-          if (res.data) {
-            await this.agent.dids.import({
-              did: `did:indy:bcovrin:${res.data.did}`,
-              overwrite: true,
-              privateKeys: [
-                {
-                  keyType: KeyType.Ed25519,
-                  privateKey: TypedArrayEncoder.fromString(data.seed),
-                },
-              ],
-            })
-            return { did: `did:indy:bcovrin:${res.data.did}` };
-          }
-        })
-        .catch((error) => {
-          console.log(error);
-        })
-    } else if ('indicio' === data.method) {
+        const res = await axios.post(BCOVRIN_REGISTER_URL, body);
 
-      const key = await this.agent.wallet.createKey({
-        privateKey: TypedArrayEncoder.fromString(data.seed),
-        keyType: KeyType.Ed25519
-      });
+        if (res) {
+          const { did } = res?.data;
+          await this.importDid(didMethod, did, data.seed);
+          return { did: `${didMethod}:${did}` };
+        }
 
+      } else if (data.method.includes('indicio')) {
 
-      const buffer = TypedArrayEncoder.fromBase58(key.publicKeyBase58);
-      const did = TypedArrayEncoder.toBase58(buffer.slice(0, 16));
+        const key = await this.agent.wallet.createKey({
+          privateKey: TypedArrayEncoder.fromString(data.seed),
+          keyType: KeyType.Ed25519
+        });
 
-      let body = {
-        network: 'testnet',
-        did,
-        verkey: TypedArrayEncoder.toBase58(buffer)
-      };
+        const buffer = TypedArrayEncoder.fromBase58(key.publicKeyBase58);
+        const did = TypedArrayEncoder.toBase58(buffer.slice(0, 16));
 
-      return await axios
-        .post('https://selfserve.indiciotech.io/nym', body)
-        .then(async (res) => {
-          if (200 === res.data.statusCode) {
+        const body = {
+          network: 'testnet',
+          did,
+          verkey: TypedArrayEncoder.toBase58(buffer)
+        };
 
-            await this.agent.dids.import({
-              did: `did:indy:indicio:${body.did}`,
-              overwrite: true,
-              privateKeys: [
-                {
-                  keyType: KeyType.Ed25519,
-                  privateKey: TypedArrayEncoder.fromString(data.seed)
-                },
-              ],
-            })
-            return { did: `did:indy:indicio:${did}` };
-          }
-        })
+        const res = await axios.post(INDICIO_NYM_URL, body);
+
+        if (res.data.statusCode === 200) {
+          await this.importDid(didMethod, body.did, data.seed);
+          return { did: `${didMethod}:${body.did}` };
+        }
+      }
+    } catch (error) {
+      return internalServerError(500, { message: `Something went wrong: ${error}` });
     }
   }
-  catch(error) {
-    return internalServerError(500, { message: `something went wrong: ${error}` })
 
-  }
-}
-
-@Post('/did/key')
-public async createDidKey(
-  @Body() didOptions: DidCreate,
-  @Res() internalServerError: TsoaResponse<500, { message: string }>
-) {
-  try {
-    const did = await this.agent.dids.create<KeyDidCreateOptions>({
-      method: 'key',
-      options: {
-        keyType: KeyType.Ed25519,
-      },
-      secret: {
-        privateKey: TypedArrayEncoder.fromString(didOptions.seed)
-      }
-    });
+  private async importDid(didMethod: string, did: string, seed: string) {
     await this.agent.dids.import({
-      did: `${did.didState.did}`,
+      did: `${didMethod}:${did}`,
       overwrite: true,
       privateKeys: [
         {
           keyType: KeyType.Ed25519,
-          privateKey: TypedArrayEncoder.fromString(didOptions.seed)
+          privateKey: TypedArrayEncoder.fromString(seed)
+        }
+      ]
+    });
+  }
+
+  @Post('/did/key')
+  public async createDidKey(
+    @Body() didOptions: DidCreate,
+    @Res() internalServerError: TsoaResponse<500, { message: string }>
+  ) {
+    try {
+      const did = await this.agent.dids.create<KeyDidCreateOptions>({
+        method: 'key',
+        options: {
+          keyType: KeyType.Ed25519,
         },
-      ],
-    });
-    return { did: `${did.didState.did}` };
-  } catch (error) {
-    return internalServerError(500, { message: `something went wrong: ${error}` })
+        secret: {
+          privateKey: TypedArrayEncoder.fromString(didOptions.seed)
+        }
+      });
+      await this.agent.dids.import({
+        did: `${did.didState.did}`,
+        overwrite: true,
+        privateKeys: [
+          {
+            keyType: KeyType.Ed25519,
+            privateKey: TypedArrayEncoder.fromString(didOptions.seed)
+          },
+        ],
+      });
+      return { did: `${did.didState.did}` };
+    } catch (error) {
+      return internalServerError(500, { message: `something went wrong: ${error}` })
+    }
   }
-}
 
-@Post('/did/web')
-public async createDidWeb(
-  @Body() didOptions: DidCreate,
-  @Res() internalServerError: TsoaResponse<500, { message: string }>
-) {
-  try {
-    const domain = didOptions.domain ? didOptions.domain : 'credebl.github.io';
-    const did = `did:web:${domain}`;
-    const keyId = `${did}#key-1`;
+  @Post('/did/web')
+  public async createDidWeb(
+    @Body() didOptions: DidCreate,
+    @Res() internalServerError: TsoaResponse<500, { message: string }>
+  ) {
+    try {
+      const domain = didOptions.domain ? didOptions.domain : 'credebl.github.io';
+      const did = `did:web:${domain}`;
+      const keyId = `${did}#key-1`;
 
-    const key = await this.agent.wallet.createKey({
-      keyType: KeyType.Ed25519,
-      privateKey: TypedArrayEncoder.fromString(didOptions.seed)
-    });
+      const key = await this.agent.wallet.createKey({
+        keyType: KeyType.Ed25519,
+        privateKey: TypedArrayEncoder.fromString(didOptions.seed)
+      });
 
-    const didDocument = new DidDocumentBuilder(did)
-      .addContext('https://w3id.org/security/suites/ed25519-2018/v1')
-      .addVerificationMethod(getEd25519VerificationKey2018({ key, id: keyId, controller: did }))
-      .addAuthentication(keyId)
-      .build();
+      const didDocument = new DidDocumentBuilder(did)
+        .addContext('https://w3id.org/security/suites/ed25519-2018/v1')
+        .addVerificationMethod(getEd25519VerificationKey2018({ key, id: keyId, controller: did }))
+        .addAuthentication(keyId)
+        .build();
 
-    await this.agent.dids.import({
-      did,
-      overwrite: true,
-      didDocument
-    });
-    return { did };
-  } catch (error) {
-    return internalServerError(500, { message: `something went wrong: ${error}` })
+      await this.agent.dids.import({
+        did,
+        overwrite: true,
+        didDocument
+      });
+      return { did };
+    } catch (error) {
+      return internalServerError(500, { message: `something went wrong: ${error}` })
+    }
   }
-}
 
 
 
-@Get('/')
-public async getDids() {
-  const createdDids = await this.agent.dids.getCreatedDids()
-  return createdDids;
-}
+  @Get('/')
+  public async getDids() {
+    const createdDids = await this.agent.dids.getCreatedDids()
+    return createdDids;
+  }
 }
