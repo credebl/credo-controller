8e933597b3bb693475c106b6abab2faa01682c37
pallavicoder <pallavi.ghule@ayanworks.com>
2024-02-07 12:25:32 +0530

    feat:added did document service inside create did web
    Signed-off-by: pallavicoder <pallavi.ghule@ayanworks.com>

--- src/controllers/multi-tenancy/MultiTenancyController.ts
+++ src/controllers/multi-tenancy/MultiTenancyController.ts
@@ -49,6 +49,7 @@ import {
   CredentialRepository,
   CredentialState,
   DidDocumentBuilder,
+  DidDocumentService,
   DidExchangeState,
   HandshakeProtocol,
   JsonTransformer,
@@ -1417,6 +1418,13 @@ export class MultiTenancyController extends Controller {
           .addContext('https://w3id.org/security/suites/ed25519-2018/v1')
           .addVerificationMethod(getEd25519VerificationKey2018({ key, id: keyId, controller: did }))
           .addAuthentication(keyId)
+          .addService(
+            new DidDocumentService({
+              id: 'did:web:pallavighule.github.io',
+              type: 'DIDCommMessaging',
+              serviceEndpoint: 'https://dev-agent.credebl.id/docs/',
+            })
+          )
           .build()
       }
       if (didOptions.keyType === KeyType.Bls12381g2) {
@@ -1424,6 +1432,13 @@ export class MultiTenancyController extends Controller {
           .addContext('https://w3id.org/security/bbs/v1')
           .addVerificationMethod(getBls12381G2Key2020({ key, id: keyId, controller: did }))
           .addAuthentication(keyId)
+          .addService(
+            new DidDocumentService({
+              id: 'did:web:pallavighule.github.io',
+              type: 'DIDCommMessaging',
+              serviceEndpoint: 'https://dev-agent.credebl.id/docs/',
+            })
+          )
           .build()
       }
 
