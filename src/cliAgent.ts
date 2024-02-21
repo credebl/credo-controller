03ee7e3dd0ba6d72c2df39bf8ed38dde76f57ddb
Sai Ranjit Tummalapalli <sairanjit.tummalapalli@ayanworks.com>
2024-02-21 15:42:38 +0530

    feat: add question answer protocol controller (#58)
    * feat: add question answer protocol controller
    
    Signed-off-by: Sai Ranjit Tummalapalli <sairanjit.tummalapalli@ayanworks.com>
    
    * fix: loosen the yarn installation
    
    Signed-off-by: Sai Ranjit Tummalapalli <sairanjit.tummalapalli@ayanworks.com>
    
    ---------
    
    Signed-off-by: Sai Ranjit Tummalapalli <sairanjit.tummalapalli@ayanworks.com>

--- src/cliAgent.ts
+++ src/cliAgent.ts
@@ -41,6 +41,7 @@ import {
   IndyVdrIndyDidRegistrar,
 } from '@aries-framework/indy-vdr'
 import { agentDependencies, HttpInboundTransport, WsInboundTransport } from '@aries-framework/node'
+import { QuestionAnswerModule } from '@aries-framework/question-answer'
 import { TenantsModule } from '@aries-framework/tenants'
 import { anoncreds } from '@hyperledger/anoncreds-nodejs'
 import { ariesAskar } from '@hyperledger/aries-askar-nodejs'
@@ -126,13 +127,16 @@ const getModules = (networkConfig: [IndyVdrPoolConfig, ...IndyVdrPoolConfig[]]) 
       registrars: [new IndyVdrIndyDidRegistrar(), new KeyDidRegistrar()],
       resolvers: [new IndyVdrIndyDidResolver(), new KeyDidResolver(), new WebDidResolver()],
     }),
+
     anoncreds: new AnonCredsModule({
       registries: [new IndyVdrAnonCredsRegistry()],
     }),
+
     // Use anoncreds-rs as anoncreds backend
-    _anoncreds: new AnonCredsRsModule({
+    anoncredsRs: new AnonCredsRsModule({
       anoncreds,
     }),
+
     connections: new ConnectionsModule({
       autoAcceptConnections: true,
     }),
@@ -166,6 +170,8 @@ const getModules = (networkConfig: [IndyVdrPoolConfig, ...IndyVdrPoolConfig[]]) 
     cache: new CacheModule({
       cache: new InMemoryLruCache({ limit: Infinity }),
     }),
+
+    questionAnswer: new QuestionAnswerModule(),
   }
 }
 
@@ -346,7 +352,7 @@ export async function runRestAgent(restConfig: AriesRestConfig) {
     token
   )
 
-  logger.info(`*** API Toekn: ${token}`)
+  logger.info(`*** API Token: ${token}`)
 
   app.listen(adminPort, () => {
     logger.info(`Successfully started server on port ${adminPort}`)
