3d2a40a5682c65311ebe39e5ff8bd3b3d600b698
tipusinghaw <tipu.singh@ayanworks.com>
2024-03-11 12:34:42 +0530

    refactor: updated the RPC url
    Signed-off-by: tipusinghaw <tipu.singh@ayanworks.com>

--- src/cliAgent.ts
+++ src/cliAgent.ts
@@ -179,7 +179,7 @@ const getModules = (networkConfig: [IndyVdrPoolConfig, ...IndyVdrPoolConfig[]]) 
       didContractAddress: '0x12513116875BB3E4F098Ce74624739Ee51bAf023',
       schemaManagerContractAddress: '0x552992e9f14b15bBd76488cD4c38c89B80259f37',
       fileServerToken: '',
-      rpcUrl: 'https://polygon-mumbai.infura.io/v3/0579d305568d404e996e49695e9272a3',
+      rpcUrl: 'https://rpc-mumbai.maticvigil.com',
       serverUrl: '',
     }),
   }
