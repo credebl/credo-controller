422a57cacaa36bc50b92254514fdb996957d9361
tipusinghaw <tipu.singh@ayanworks.com>
2024-03-12 12:47:38 +0530

    feat: multi DID support and decuple wallet setup
    Signed-off-by: tipusinghaw <tipu.singh@ayanworks.com>

--- src/enums/enum.ts
+++ src/enums/enum.ts
@@ -2,3 +2,22 @@ export enum CredentialEnum {
   Finished = 'finished',
   Action = 'action',
 }
+
+export enum Role {
+  Author = 'author',
+  Endorser = 'endorser',
+}
+
+export enum DidMethod {
+  Indy = 'indy',
+  Key = 'key',
+  Web = 'web',
+  Polygon = 'polygon',
+}
+
+export enum Network {
+  Bcovrin_Testnet = 'bcovrin:testnet',
+  Indicio_Testnet = 'indicio:testnet',
+  Indicio_Demonet = 'indicio:demonet',
+  Indicio_Mainnet = 'indicio:mainnet',
+}
