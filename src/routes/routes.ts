c9b54da5a0d6eec14d60851d6c7d3a8b6a82499e
Krishna <krishna.waske@ayanworks.com>
2024-04-10 17:04:28 +0530

    fix: agent auth
    Signed-off-by: Krishna <krishna.waske@ayanworks.com>

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -4081,7 +4081,7 @@ export function RegisterRoutes(app: Router) {
             }
         });
         // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
-        app.get('/agent/info',
+        app.get('/agent',
             ...(fetchMiddlewares<RequestHandler>(AgentController)),
             ...(fetchMiddlewares<RequestHandler>(AgentController.prototype.getAgentInfo)),
 
