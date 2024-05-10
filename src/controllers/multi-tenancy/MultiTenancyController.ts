d065943a655cf3b1546a6e3af6ee9e06d0af300d
tipusinghaw <tipu.singh@ayanworks.com>
2024-05-10 16:01:32 +0530

    feat: added schema url and logic to create config.json
    Signed-off-by: tipusinghaw <tipu.singh@ayanworks.com>

--- src/controllers/multi-tenancy/MultiTenancyController.ts
+++ src/controllers/multi-tenancy/MultiTenancyController.ts
@@ -1,6 +1,6 @@
 import type { RestAgentModules, RestMultiTenantAgentModules } from '../../cliAgent'
 import type { Version } from '../examples'
-import type { RecipientKeyOption } from '../types'
+import type { RecipientKeyOption, SchemaMetadata } from '../types'
 import type { PolygonDidCreateOptions } from '@ayanworks/credo-polygon-w3c-module/build/dids'
 import type {
   AcceptProofRequestOptions,
@@ -49,6 +49,7 @@ import {
 } from '@credo-ts/core'
 import { QuestionAnswerRole, QuestionAnswerState } from '@credo-ts/question-answer'
 import axios from 'axios'
+import * as fs from 'fs'
 
 import { CredentialEnum, DidMethod, Network, Role } from '../../enums/enum'
 import { BCOVRIN_REGISTER_URL, INDICIO_NYM_URL } from '../../utils/util'
@@ -992,23 +993,39 @@ export class MultiTenancyController extends Controller {
     },
     @Path('tenantId') tenantId: string,
     @Res() internalServerError: TsoaResponse<500, { message: string }>
-  ): Promise<unknown> {
+  ): Promise<SchemaMetadata> {
     try {
-      let schemaResponse
       const { did, schemaName, schema } = createSchemaRequest
       if (!did || !schemaName || !schema) {
         throw Error('One or more parameters are empty or undefined.')
       }
 
-      await this.agent.modules.tenants.withTenantAgent({ tenantId }, async (tenantAgent) => {
-        //need to add the return type after adding the scham URL
-        schemaResponse = await tenantAgent.modules.polygon.createSchema({
+      const schemaResponse = await this.agent.modules.tenants.withTenantAgent({ tenantId }, async (tenantAgent) => {
+        return await tenantAgent.modules.polygon.createSchema({
           did,
           schemaName,
           schema,
         })
       })
-      return schemaResponse
+
+      const configFileData = fs.readFileSync('config.json', 'utf-8')
+      const config = JSON.parse(configFileData)
+
+      if (config.schemaFileServerURL.lenghth > 0 && config.schemaFileServerURL != undefined) {
+        throw new Error('Please provide valid schema file server URL')
+      }
+
+      if (!schemaResponse?.schemaId) {
+        throw new Error('Invalid schema response')
+      }
+      const schemaPayload: SchemaMetadata = {
+        schemaUrl: config.schemaFileServerURL + schemaResponse?.schemaId,
+        did: schemaResponse?.did,
+        schemaId: schemaResponse?.schemaId,
+        schemaTxnHash: schemaResponse?.resourceTxnHash,
+      }
+
+      return schemaPayload
     } catch (error) {
       return internalServerError(500, { message: `something went wrong: ${error}` })
     }
