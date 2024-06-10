6bf86f30903c8013c83f87e1623a7274c5febd8e
tipusinghaw <tipu.singh@ayanworks.com>
2024-06-10 15:07:49 +0530

    chore: updated contract address
    Signed-off-by: tipusinghaw <tipu.singh@ayanworks.com>

--- src/cliAgent.ts
+++ src/cliAgent.ts
@@ -177,8 +177,8 @@ const getModules = (networkConfig: [IndyVdrPoolConfig, ...IndyVdrPoolConfig[]]) 
 
     questionAnswer: new QuestionAnswerModule(),
     polygon: new PolygonModule({
-      didContractAddress: '0x1adeA199dCf07E17232415Cb232442BE52517Add',
-      schemaManagerContractAddress: '0x289c7Bd4C7d38cC54bff370d6f9f01b74Df51b11',
+      didContractAddress: '0xcB80F37eDD2bE3570c6C9D5B0888614E04E1e49E',
+      schemaManagerContractAddress: '0x4742d43C2dFCa5a1d4238240Afa8547Daf87Ee7a',
       fileServerToken:
         'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJBeWFuV29ya3MiLCJpZCI6ImNhZDI3ZjhjLTMyNWYtNDRmZC04ZmZkLWExNGNhZTY3NTMyMSJ9.I3IR7abjWbfStnxzn1BhxhV0OEzt1x3mULjDdUcgWHk',
       rpcUrl: 'https://rpc-amoy.polygon.technology',
