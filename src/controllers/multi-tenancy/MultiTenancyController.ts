419ce84eb8608147785c915ec5fb17c20b2b251c
tipusinghaw <tipu.singh@ayanworks.com>
2024-06-21 14:55:18 +0530

    refactor:added condition for insufficient balance
    Signed-off-by: tipusinghaw <tipu.singh@ayanworks.com>

--- src/controllers/multi-tenancy/MultiTenancyController.ts
+++ src/controllers/multi-tenancy/MultiTenancyController.ts
@@ -993,7 +993,7 @@ export class MultiTenancyController extends Controller {
     },
     @Path('tenantId') tenantId: string,
     @Res() internalServerError: TsoaResponse<500, { message: string }>
-  ): Promise<SchemaMetadata> {
+  ): Promise<SchemaMetadata | string> {
     try {
       const { did, schemaName, schema } = createSchemaRequest
       if (!did || !schemaName || !schema) {
@@ -1007,6 +1007,14 @@ export class MultiTenancyController extends Controller {
           schema,
         })
       })
+      if (schemaResponse.schemaState?.state === 'failed') {
+        const reason = schemaResponse.schemaState?.reason?.toLowerCase()
+        if (reason && reason.includes('insufficient') && reason.includes('funds')) {
+          throw new Error('Insufficient funds to the address, Please add funds to perform this operation')
+        } else {
+          throw new Error(schemaResponse.schemaState?.reason)
+        }
+      }
 
       const configFileData = fs.readFileSync('config.json', 'utf-8')
       const config = JSON.parse(configFileData)
