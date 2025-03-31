e03573d3722c3444510b3ed8f63668f808a02a1b
Tipu_Singh <tipu.singh@ayanworks.com>
2025-03-31 09:48:33 +0530

    feat:w3c-revocation
    Signed-off-by: Tipu_Singh <tipu.singh@ayanworks.com>
    Signed-off-by: Sahil Kamble <sahil.kamble@ayanworks.com>

--- src/controllers/credentials/CredentialController.ts
+++ src/controllers/credentials/CredentialController.ts
@@ -13,8 +13,11 @@ import {
   CredentialRole,
   createPeerDidDocumentFromServices,
   PeerDidNumAlgo,
+  ClaimFormat,
 } from '@credo-ts/core'
+import axios from 'axios'
 import { injectable } from 'tsyringe'
+import { v4 as uuidv4 } from 'uuid'
 
 import ErrorHandlingService from '../../errorHandlingService'
 import { CredentialExchangeRecordExample, RecordId } from '../examples'
@@ -30,6 +33,15 @@ import {
   ThreadId,
 } from '../types'
 
+import { initialBitsEncoded } from 'src/constants'
+import {
+  CredentialContext,
+  CredentialStatusListType,
+  CredentialType,
+  RevocationListType,
+  SignatureType,
+} from 'src/enums/enum'
+import { BadRequestError, InternalServerError } from 'src/errors'
 import { Body, Controller, Get, Path, Post, Route, Tags, Example, Query, Security } from 'tsoa'
 
 @Tags('Credentials')
@@ -310,6 +322,139 @@ export class CredentialController extends Controller {
     try {
       const credentialDetails = await this.agent.credentials.getFormatData(credentialRecordId)
       return credentialDetails
+    } catch (error) {
+      throw ErrorHandlingService.handle(error)
+    }
+  }
+
+  /**
+   * Create bitstring status list credential
+   *
+   * @param tenantId Id of the tenant
+   * @param request BSLC required details
+   */
+  @Security('apiKey')
+  @Post('/create-bslc')
+  public async createBitstringStatusListCredential(
+    @Path('tenantId') tenantId: string,
+    @Body() request: { issuerDID: string; statusPurpose: string; verificationMethod: string }
+  ) {
+    try {
+      const { issuerDID, statusPurpose, verificationMethod } = request
+      const bslcId = uuidv4()
+      const credentialpayload = {
+        '@context': [`${CredentialContext.V1}`, `${CredentialContext.V2}`],
+        id: `${process.env.BSLC_SERVER_URL}/bitstring/${bslcId}`,
+        type: [`${CredentialType.VerifiableCredential}`, `${CredentialType.BitstringStatusListCredential}`],
+        issuer: {
+          id: issuerDID as string,
+        },
+        issuanceDate: new Date().toISOString(),
+        credentialSubject: {
+          id: `${process.env.BSLC_SERVER_URL}/bitstring/${bslcId}`,
+          type: `${RevocationListType.Bitstring}`,
+          statusPurpose: statusPurpose,
+          encodedList: initialBitsEncoded,
+        },
+        credentialStatus: {
+          id: `${process.env.BSLC_SERVER_URL}/bitstring/${bslcId}`,
+          type: CredentialStatusListType.CredentialStatusList2017,
+        },
+      }
+
+      let signedCredential
+      // Step 2: Sign the payload
+      try {
+        //TODO: Add correct type here
+        signedCredential = await this.agent.w3cCredentials.signCredential({
+          credential: credentialpayload,
+          format: ClaimFormat.LdpVc,
+          proofType: SignatureType.Ed25519Signature2018,
+          verificationMethod,
+        })
+      } catch (signingError) {
+        throw new InternalServerError(`Failed to sign the BitstringStatusListCredential: ${signingError}`)
+      }
+      // Step 3: Upload the signed payload to the server
+      const serverUrl = process.env.BSLC_SERVER_URL
+      if (!serverUrl) {
+        throw new Error('BSLC_SERVER_URL is not defined in the environment variables')
+      }
+
+      const token = process.env.BSLC_SERVER_TOKEN
+      if (!token) {
+        throw new Error('BSLC_SERVER_TOKEN is not defined in the environment variables')
+      }
+      const url = `${serverUrl}/bitstring`
+      const bslcPayload = {
+        id: bslcId,
+        bslcObject: signedCredential,
+      }
+      try {
+        const response = await axios.post(url, bslcPayload, {
+          headers: {
+            Accept: '*/*',
+            Authorization: `Bearer ${token}`,
+            'Content-Type': 'application/json',
+          },
+        })
+
+        if (response.status !== 200) {
+          throw new Error('Failed to upload the signed BitstringStatusListCredential')
+        }
+      } catch (error) {
+        throw new InternalServerError(`Error uploading the BitstringStatusListCredential: ${error}`)
+      }
+      return signedCredential
+    } catch (error) {
+      throw ErrorHandlingService.handle(error)
+    }
+  }
+
+  @Security('apiKey')
+  @Post('/get-empty-index/:BSLCUrl')
+  public async getEmptyIndexForBSLC(@Path('BSLCUrl') BSLCUrl: string) {
+    try {
+      if (!BSLCUrl) {
+        throw new BadRequestError('BSLCUrl is required')
+      }
+
+      const response = await axios.get(BSLCUrl)
+      if (response.status !== 200) {
+        throw new Error('Failed to fetch the BitstringStatusListCredential')
+      }
+
+      const credential = response.data
+      const encodedList = credential?.credentialSubject?.claims.encodedList
+      if (!encodedList) {
+        throw new Error('Encoded list not found in the credential')
+      }
+
+      let bitstring
+      try {
+        const compressedData = Buffer.from(encodedList, 'base64').toString('binary')
+        bitstring = Array.from(compressedData)
+          .map((byte) => byte.padStart(8, '0'))
+          .join('')
+      } catch (error) {
+        throw new Error('Failed to decompress and process the encoded list')
+      }
+
+      const unusedIndexes = []
+      for (let i = 0; i < bitstring.length; i++) {
+        if (bitstring[i] === '0') {
+          unusedIndexes.push(i)
+        }
+      }
+      //TODO: add logic to filter from used indexs, for now returning random index with bit status as 0.
+      if (unusedIndexes.length === 0) {
+        throw new Error('No unused index found in the BitstringStatusList')
+      }
+
+      const randomIndex = unusedIndexes[Math.floor(Math.random() * unusedIndexes.length)]
+      return {
+        index: randomIndex,
+      }
     } catch (error) {
       throw ErrorHandlingService.handle(error)
     }
