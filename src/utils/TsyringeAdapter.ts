44167dd8a2fd29e629cb9479cb0a17ebf14d9528
Krishna Waske <krishna.waske@ayanworks.com>
2025-07-01 16:06:05 +0530

    fix: remove unnecessary files
    Signed-off-by: Krishna Waske <krishna.waske@ayanworks.com>

--- src/utils/TsyringeAdapter.ts
+++
@@ -1,9 +0,0 @@
-import type { IocContainer } from '@tsoa/runtime'
-
-import { container } from 'tsyringe'
-
-export const iocContainer: IocContainer = {
-  get: <T>(controller: any): T => {
-    return container.resolve<T>(controller)
-  },
-}
