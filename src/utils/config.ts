751dfa12a155fea75f04f61872b98847350ff52d
Krishna Waske <krishna.waske@ayanworks.com>
2025-10-16 18:09:25 +0530

    feat: add appropriate warning for incorrect custom document loader initiation
    Signed-off-by: Krishna Waske <krishna.waske@ayanworks.com>

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,4 +1,13 @@
 export const IsCustomDocumentLoaderEnabled = (): boolean => {
   const flag = process.env.ENABLE_CUSTOM_DOCUMENT_LOADER ?? 'false'
-  return flag.toLowerCase() === 'true'
+  const isCustomDocumentLoaderEnabled = flag.toLowerCase() === 'true'
+
+  if (isCustomDocumentLoaderEnabled) {
+    if (!process.env.DEPRECATED_DOMAIN || !process.env.CURRENT_DOMAIN) {
+      console.error(`Custom document loader for the agent is enabled but the deprecated domain and updated domain is not set`)
+    }
+    console.warn(`Custom document loader for the agent is enabled. Resolution of all URLs from the deprecated domain(${process.env.DEPRECATED_DOMAIN}) will actually be resolved from the current, updated domain(${process.env.CURRENT_DOMAIN})`)
+  }
+
+  return isCustomDocumentLoaderEnabled
 }
