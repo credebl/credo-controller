5b1ceb7f3f0d5f1813ab71842deb02d70c35d2c6
Krishna Waske <krishna.waske@ayanworks.com>
2025-10-16 18:11:25 +0530

    fix: formatting
    Signed-off-by: Krishna Waske <krishna.waske@ayanworks.com>

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -4,9 +4,13 @@ export const IsCustomDocumentLoaderEnabled = (): boolean => {
 
   if (isCustomDocumentLoaderEnabled) {
     if (!process.env.DEPRECATED_DOMAIN || !process.env.CURRENT_DOMAIN) {
-      console.error(`Custom document loader for the agent is enabled but the deprecated domain and updated domain is not set`)
+      console.error(
+        `Custom document loader for the agent is enabled but the deprecated domain and updated domain is not set`,
+      )
     }
-    console.warn(`Custom document loader for the agent is enabled. Resolution of all URLs from the deprecated domain(${process.env.DEPRECATED_DOMAIN}) will actually be resolved from the current, updated domain(${process.env.CURRENT_DOMAIN})`)
+    console.warn(
+      `Custom document loader for the agent is enabled. Resolution of all URLs from the deprecated domain(${process.env.DEPRECATED_DOMAIN}) will actually be resolved from the current, updated domain(${process.env.CURRENT_DOMAIN})`,
+    )
   }
 
   return isCustomDocumentLoaderEnabled
