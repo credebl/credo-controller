12e9d9158d9991abf1edc0169543cd86ff18c128
Rinkal Bhojani <rinkal.bhojani@ayanworks.com>
2025-10-30 16:29:15 +0530

    fix: Added getTrustedCert function and refactored code
    Signed-off-by: Rinkal Bhojani <rinkal.bhojani@ayanworks.com>

--- src/controllers/openid4vc/issuance-sessions/issuance-sessions.service.ts
+++ src/controllers/openid4vc/issuance-sessions/issuance-sessions.service.ts
@@ -1,7 +1,7 @@
 import type { OpenId4VcIssuanceSessionsCreateOffer } from '../types/issuer.types'
-import { OpenId4VciCredentialFormatProfile, type OpenId4VcIssuanceSessionState } from '@credo-ts/openid4vc'
 import type { Request as Req } from 'express'
 
+import { type OpenId4VcIssuanceSessionState } from '@credo-ts/openid4vc'
 import { OpenId4VcIssuanceSessionRepository } from '@credo-ts/openid4vc/build/openid4vc-issuer/repository'
 
 import { SignerMethod } from '../../../enums/enum'
@@ -43,14 +43,14 @@ class IssuanceSessionsService {
         )
       }
 
-      const currentVct = cred.payload && 'vct' in cred.payload ? cred.payload.vct : undefined;
+      const currentVct = cred.payload && 'vct' in cred.payload ? cred.payload.vct : undefined
       return {
-    ...cred,
-    payload: {
-      ...cred.payload,
-      vct: currentVct ?? (typeof supported.vct === 'string' ? supported.vct : undefined),
-    },
-  }
+        ...cred,
+        payload: {
+          ...cred.payload,
+          vct: currentVct ?? (typeof supported.vct === 'string' ? supported.vct : undefined),
+        },
+      }
     })
 
     options.issuanceMetadata ||= {}
